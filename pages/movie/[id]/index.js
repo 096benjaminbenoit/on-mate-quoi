21e8fcc8b97e6f0575db493ddad748222ecb5a33
096benjaminbenoit <096benjaminbenoit@gmail.com>
2023-04-17 19:21:10 +0200

    apos fixed

--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -26,7 +26,7 @@ const Movie = ({ movie }) => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                     </svg>
-                    <span className='ps-2'>Retour à l'accueil</span>
+                    <span className='ps-2'>Retour à l&apos;accueil</span>
                 </Link>
                 <div className="lg:max-w-lg">
                 <p className="text-base font-semibold leading-7 text-indigo-600">{movie.release_date}</p>
